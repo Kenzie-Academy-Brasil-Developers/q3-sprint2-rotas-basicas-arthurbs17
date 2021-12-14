4f9d60407b5325a6139977e21849ea12d945f4a2
Arthur Sarmento <arthurbs17@gmail.com>
2021-12-14 13:47:27 -0300

    Tasks done

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-# Código do dev aqui
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return {"data": "Hello Flask!"}
+
+@app.route('/current_datetime', methods=['GET'])
+def current_datetime():
+    datetime_now = datetime.now()
+    date_return = dict()
+    date_return['current_datetime'] = f'{datetime_now.strftime("%d/%m/%Y %H:%M:%S %p")}'
+    if datetime_now.hour < 12:
+        date_return['message'] = 'Bom dia!'
+    if datetime_now.hour >= 12 and datetime_now.hour < 18:
+        date_return['message'] = 'Boa tarde!'
+    if datetime_now.hour > 18:
+        date_return['message'] = 'Boa noite!'
+    return date_return
+
+print(home())
+print(current_datetime())
